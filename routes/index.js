f4c46643ec10ecd3412c374e4508f11abc54c266
Juan Pinilla <juan@kamikazepanda.com>
2012-09-24 17:25:55 -0500

    Probando angularJS

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'getUserMedia' });
 };
+
+exports.chat = function(req, res){
+  res.render('chat', { title: 'getUserMedia' });
+};
